d80787445f17c39378a11b36b572021b19f96092
Leonid Marushevskiy <leonid.marushevskiy@engagepoint.com>
2017-07-22 21:35:09 -0700

    Some changes to integrate api-core

--- src/main/java/gov/ca/cwds/geo/inject/DataAccessModule.java
+++ src/main/java/gov/ca/cwds/geo/inject/DataAccessModule.java
@@ -1,11 +1,19 @@
 package gov.ca.cwds.geo.inject;
 
+import com.codahale.metrics.Reporter;
+import com.google.common.collect.ImmutableList;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import gov.ca.cwds.data.persistence.cms.SystemCode;
+import gov.ca.cwds.data.persistence.cms.SystemMeta;
 import gov.ca.cwds.geo.GeoServicesApiConfiguration;
 import gov.ca.cwds.geo.SmartyStreetsConfig;
 import gov.ca.cwds.geo.persistence.dao.SmartyStreetsDAO;
+import gov.ca.cwds.inject.CmsSessionFactory;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.setup.Bootstrap;
+import org.hibernate.SessionFactory;
 
 /**
  * DI (dependency injection) setup for data access objects (DAO).
@@ -14,12 +22,28 @@ import io.dropwizard.setup.Bootstrap;
  */
 public class DataAccessModule extends AbstractModule {
 
+  private final HibernateBundle<GeoServicesApiConfiguration> cmsHibernateBundle =
+      new HibernateBundle<GeoServicesApiConfiguration>(Reporter.class) {
+        @Override
+        public DataSourceFactory getDataSourceFactory(GeoServicesApiConfiguration configuration) {
+          return configuration.getCmsDataSourceFactory();
+        }
+
+        @Override
+        public String name() {
+          return "cms";
+        }
+      };
+
+
   /**
    * Constructor takes the API config.
    *
    * @param bootstrap the ApiConfiguration
    */
-  public DataAccessModule(Bootstrap<GeoServicesApiConfiguration> bootstrap) {}
+  public DataAccessModule(Bootstrap<GeoServicesApiConfiguration> bootstrap) {
+    bootstrap.addBundle(cmsHibernateBundle);
+  }
 
   /**
    * {@inheritDoc}
@@ -36,4 +60,11 @@ public class DataAccessModule extends AbstractModule {
       GeoServicesApiConfiguration geoServiceApiConfiguration) {
     return geoServiceApiConfiguration.getSmartyStreetsConfig();
   }
+
+  @Provides
+  @CmsSessionFactory
+  SessionFactory cmsSessionFactory() {
+    return cmsHibernateBundle.getSessionFactory();
+  }
+
 }
--- src/main/java/gov/ca/cwds/geo/inject/ServicesModule.java
+++ src/main/java/gov/ca/cwds/geo/inject/ServicesModule.java
@@ -1,7 +1,13 @@
 package gov.ca.cwds.geo.inject;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import gov.ca.cwds.data.cms.SystemCodeDao;
+import gov.ca.cwds.data.cms.SystemMetaDao;
 import gov.ca.cwds.geo.service.AddressValidationService;
+import gov.ca.cwds.rest.services.cms.CachingSystemCodeService;
+import gov.ca.cwds.rest.services.cms.SystemCodeService;
+import gov.ca.cwds.rest.api.domain.cms.SystemCodeCache;
 
 /**
  * Identifies all GEO Services API business layer (services) classes available for dependency
@@ -19,5 +25,19 @@ public class ServicesModule extends AbstractModule {
   @Override
   protected void configure() {
     bind(AddressValidationService.class);
+  }
+
+  @Provides
+  public SystemCodeService provideSystemCodeService(SystemCodeDao systemCodeDao,
+      SystemMetaDao systemMetaDao) {
+    final long secondsToRefreshCache = 15L * 24 * 60 * 60; // 15 days
+    return new CachingSystemCodeService(systemCodeDao, systemMetaDao, secondsToRefreshCache, false);
+  }
+
+  @Provides
+  public SystemCodeCache provideSystemCodeCache(SystemCodeService systemCodeService) {
+    SystemCodeCache systemCodeCache = (SystemCodeCache) systemCodeService;
+    systemCodeCache.register();
+    return systemCodeCache;
   }
 }
