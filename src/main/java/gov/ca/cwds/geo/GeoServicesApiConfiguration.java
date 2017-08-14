42188e3996069b5b8c19a1a9826b74fba7eae302
Leonid Marushevskiy <leonid.marushevskiy@engagepoint.com>
2017-08-13 23:30:39 -0700

    #150111356: new version of API core integrated

--- src/main/java/gov/ca/cwds/geo/GeoServicesApiConfiguration.java
+++ src/main/java/gov/ca/cwds/geo/GeoServicesApiConfiguration.java
@@ -1,10 +1,10 @@
 package gov.ca.cwds.geo;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import gov.ca.cwds.rest.BaseApiConfiguration;
+import gov.ca.cwds.rest.MinimalApiConfiguration;
 import javax.annotation.Nullable;
 
-public class GeoServicesApiConfiguration extends BaseApiConfiguration {
+public class GeoServicesApiConfiguration extends MinimalApiConfiguration {
 
   @Nullable private SmartyStreetsConfig smartyStreetsConfig;
 
