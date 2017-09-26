3e5adaccfc2b63746a773d104286e5be181cb0b2
Leonid Marushevskiy <leonid.marushevskiy@engagepoint.com>
2017-09-26 14:47:33 -0700

    #150524704: Address suggestion test

--- src/test/java/gov/ca/cwds/geo/web/rest/AddressResourceTest.java
+++ src/test/java/gov/ca/cwds/geo/web/rest/AddressResourceTest.java
@@ -3,6 +3,7 @@ package gov.ca.cwds.geo.web.rest;
 import static gov.ca.cwds.geo.Constants.ADDRESS;
 import static gov.ca.cwds.geo.Constants.API.SYSTEM_INFORMATION;
 import static gov.ca.cwds.geo.Constants.LOOKUP_ZIP_CODE;
+import static gov.ca.cwds.geo.Constants.SUGGEST;
 import static gov.ca.cwds.geo.Constants.VALIDATE_SINGLE;
 import static gov.ca.cwds.geo.Constants.ZIP_CODE;
 import static gov.ca.cwds.geo.web.rest.AssertFixtureUtils.assertResponseByFixturePath;
@@ -45,5 +46,14 @@ public class AddressResourceTest extends BaseApiIntegrationTest {
         Response response = invocation.get(Response.class);
         assertResponseByFixturePath(response, "fixtures/addressLookupResponse.json");
     }
+
+  @Test
+  public void testAddressSuggestion() throws Exception {
+    String suggestion = "1489 Black Bear St";
+    WebTarget target = clientTestRule.target(ADDRESS + "/" + SUGGEST + "/" + suggestion);
+    Invocation.Builder invocation = target.request(MediaType.APPLICATION_JSON);
+    Response response = invocation.get(Response.class);
+    assertResponseByFixturePath(response, "fixtures/addressSuggestResponse.json");
+  }
 
 }
