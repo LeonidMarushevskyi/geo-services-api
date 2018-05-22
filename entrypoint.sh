fc8243bb353b0f5277d7b887a6fce1c073a37d15
Oleksandr Kuznetsov <oleksander.kuznetsov@engagepoint.com>
2018-05-22 10:46:01 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,10 @@ then
   API_CONFIG="geo-services-api.yml"
 fi
 
+if [ -x /paramfolder/parameters.sh ]; then
+    source /paramfolder/parameters.sh
+fi
+
 if [ -f /opt/newrelic/newrelic.yml ]; then
     java -javaagent:/opt/newrelic/newrelic.jar  ${JAVA_OPTS} -jar geo-services-api.jar server $API_CONFIG
 else
